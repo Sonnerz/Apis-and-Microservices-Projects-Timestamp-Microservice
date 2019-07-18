a9811338eacd6facb30ebdf53fb8d4b0c9d1cf7c
Glitch (vagabond-teeth) <none>
2019-07-18 12:49:25 +0000

    😀🚿 initial commit

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config()
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,8 +27,26 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+// ############ MY PROJECT CODE #####
+
+app.get('/api/timestamp/:date_string?', function (req, res) {
+  const paramDate = req.params.date_string; // get :date_string from route
+  const dateNow = new Date(); // now date for undefined param
+  //console.log(isNaN(paramDate)) // 2015-12-25 true, 1450137600 false
+
+  if (paramDate == undefined) {
+    res.json({ "unix": dateNow.getTime(), "utc": dateNow.toUTCString() })
+  } else if (isNaN(paramDate) && new Date(paramDate).toString() != "Invalid Date") { // 2015-12-25
+    res.json({ "unix": new Date(paramDate).getTime(), "utc": new Date(paramDate).toUTCString() })
+  } else if (!isNaN(paramDate) && new Date(paramDate * 1000).toString() != "Invalid Date") { // 1450137600
+    res.json({ "unix": paramDate, "utc": new Date(paramDate * 1000).toUTCString() })
+  } else {
+    res.json({ "error": "Invalid Date" })
+  }
+}) 
